06711166351d18aa17d6dc6db1dfd9833b129b96
Abinash <amohapatra458@gmail.com>
2019-12-04 16:47:53 +0530

    express generator REST API

---
+++ routes/leaderRouter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end("will send all the leaders to you!");
+})
+.post((req, res, next) => {
+    res.end("Will add the leader: " + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on leaders!");
+})
+.delete((req, res, next) => {
+    res.end("Deleting all the leaders!")
+})
+
+
+leaderRouter.route('/:leaderId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end("will send details of the leader: " + req.params.leaderId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /leaders/' + req.params.leaderId);
+})
+.put((req, res, next) => {
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end("will update the leader: " + req.body.name + " with details: " + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end("Deleting leaders " + req.params.leaderId);
+});
+
+module.exports = leaderRouter;
