8f6fe0b1cf722caa73f1b50432bad33dde4d434d
Abinash <amohapatra458@gmail.com>
2019-12-06 17:11:33 +0530

    passport jwt

--- authenticate.js
+++ authenticate.js
@@ -1,9 +1,43 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken');
+
+var config = require('./config');
 
 //the authenticate function is provided by the 'passport-local-mongoose' plugins.
 //it adds this method to user schema and the model. this provieds the authentication for the local strategy.
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function(user) {
+    //helps us to create json web token
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+};
+
+
+//the opts means options specifies how the json web token should be extracted from the incoming request message
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts, 
+    (jwt_payload, done) => {
+        console.log("JWT payload: " + jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            else if(user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
