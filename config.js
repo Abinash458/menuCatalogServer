c7d8673da65364dd83c032c2d77ec012e6a885d4
Abinash <amohapatra458@gmail.com>
2019-12-09 12:33:36 +0530

    passport facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '779186885888785',
+        clientSecret: '6fca285f9b4dae4c407daa664eb18a03'
+    }
 }
